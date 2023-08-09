cef9f81063d76b91af9db6feaa2778d53f9cc3be
Nicksonkipkew <141273661+Nicksonkipkew@users.noreply.github.com>
2023-08-09 21:34:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import sweetviz as sv
+import pandas as pd
+
+
+# Load the dataset
+dataset_path = "heart_2020_cleaned.csv" 
+data = pd.read_csv(dataset_path)
+
+# Generate the analysis report using Sweetviz
+report = sv.analyze(data)
+
+# Save the report as an HTML file
+report_path = "report.html"
+report.show_html(report_path)
